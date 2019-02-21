556eae1663ee182e3dae8a20f218f24b9dd3bd96
DiegoLealB <diegolealb@outlook.com>
2019-02-21 13:28:41 -0500

    Links user and txHash in mongo

---
+++ backend/routes/index.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var router = express.Router();
+var axios = require('axios');
+var mongoose = require("mongoose");
+
+var Image = require('../public/models/schema');
+
+const CLOUDINARY_URL="https://684518815981178:28_JNXsgqpczG-2Rl4cCd4oZ1PM@api.cloudinary.com/v1_1/diegolealb/resources/image";
+let gallery;
+
+// Get the gallery before page loads
+getGallery();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  getGallery();
+  res.json(gallery);
+});
+
+router.post('/api/createRecord', async(req, res) => {
+  console.log(req.body)
+  let image = new Image({
+    cloudinaryPayload: req.body.info,
+    txHash: req.body.txHash,
+    user: req.body.user,
+  })
+  image.save();
+})
+
+router.post('/api/updateRecord', async(req, res) => {
+  image.findOne({})
+})
+
+module.exports = router;
+
+function getGallery() {
+  axios.get(CLOUDINARY_URL)
+    .then(res => {
+      gallery = res.data.resources;
+      console.log("Refreshed image gallery from cloudinary");
+    })
+    .catch(err => {
+      console.error(err);
+    });
+}
+
