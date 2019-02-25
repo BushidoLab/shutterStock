f161ee1e04c1afa6988b1ed4b94cf04ef39e3561
DiegoLealB <diegolealb@outlook.com>
2019-02-25 13:09:35 -0500

    FileUpload works properly

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,11 +1,11 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import '../global-styles.css';
 
 function Header() {
     return (
-        <Fragment>
-            <h1 style={{color: "white", position: "absolute", top: "10px", right: '45%' }}>Photo Gallery</h1>
-        </Fragment>
+        <div>    
+            <h1 className="headerText">Photo Gallery</h1>
+        </div>
     )
 }
 
--- frontend/src/components/simpleUpload.js
+++ frontend/src/components/simpleUpload.js
@@ -1,32 +1,78 @@
 import React from 'react';
-import { Form, Upload, Button, Icon } from 'antd';
+import { Form, Upload, Button, Icon, message } from 'antd';
 import '../global-styles.css';
-import axios from 'axios';
+// import axios from 'axios';
+import reqwest from 'reqwest';
 
 class SimpleUpload extends React.Component {
-    handleSubmit = (e) => {
-        e.preventDefault();
-        this.props.form.validateFields((err, values) => {
-          if (!err) {
-            console.log('Received values of form: ', values);
-          }
-        });
+    state = {
+        file: '',
+        uploading: false,
     }
 
-    normFile = (e) => {
-        // if (Array.isArray(e)) {
-        //   return e;
-        // }
-        console.log(e.file);
-        axios.post('http://localhost:8080/api/postImage', e.file)
-            .then(res => {
-                console.log(res);
-            })
-            .catch(err => {
-                console.warn(err);
-            })
-        return e && e.fileList;
-    }
+    handleUpload = () => {
+        const { file } = this.state;
+        const formData = new FormData();
+        const url = "https://api.cloudinary.com/v1_1/diegolealb/upload";
+
+        formData.append('file', file);
+        formData.append('upload_preset', 'default_preset');
+
+        this.setState({
+            uploading: true,
+        });
+        
+        reqwest({
+            url: url,
+            method: 'post',
+            processData: false,
+            data: formData,
+            success: () => {
+                this.setState({
+                    file: '',
+                    uploading: false,
+                });
+                message.success('Uploaded successfully');
+            },
+            error: () => {
+                this.setState({
+                    uploading: false,
+                });
+                message.error('Upload failed')
+            }
+        });
+    };
+
+    getMetamask = async () => {
+        if (typeof window.ethereum !== 'undefined' || (typeof window.web3 !== 'undefined')) {
+            // const provider = window['ethereum'] || window.web3.currentProvider
+        }
+        let accounts = await window.ethereum.enable();
+        return accounts;
+    };
+
+    sendTransaction = async (accounts) => {
+        let params = [{
+            "from": accounts[0],
+            "to": "0x4fc03322307e64842c841198750be5839e815ca7",
+            "gas": "30400",
+            "gasPrice": "1000",
+            "value": "1",
+        }];
+        
+        let txHash = await window.ethereum.sendAsync({
+            method: 'eth_sendTransaction',
+            params: params,
+            from: accounts[0], // Provide the user's account to use.
+        }, (err, res) => {
+            if (err) {
+                console.err(err)
+                return;
+            };
+            console.log(res);
+            // this.postRequest(result, res.result, accounts[0]); 
+        })
+    };
 
     render() {
         const formItemLayout = {
@@ -34,19 +80,48 @@ class SimpleUpload extends React.Component {
             wrapperCol: { span: 14 },
         };
         const { getFieldDecorator } = this.props.form;
+        const { uploading, file } = this.state;
+        const props = {
+            onRemove: () => {
+                this.setState({
+                    file: '',
+                })
+            },
+            beforeUpload: (file) => {
+                this.getMetamask()
+                    .then(accounts => {
+                        this.sendTransaction(accounts);
+                    })
+                this.setState({
+                    file: file,
+                });
+                return false;
+            },
+            file,
+        };
 
         return (
-            <Form onSubmit={this.handleSubmit} className="formContainer">
+            <Form onSubmit={this.handleUpload} className="formContainer">
                 <Form.Item {...formItemLayout} >
                     {getFieldDecorator('upload', {
                         valuePropName: 'fileList',
-                        getValueFromEvent: this.normFile,
                     })(
-                        <Upload action="http://localhost:8080/api/postImage" listType="picture">
-                            <Button>
-                                <Icon type="upload" /> Upload images
+                        <React.Fragment>
+                            <Upload {...props} listType="picture" accept=".jpg, .jpeg, .png">
+                                <Button>
+                                    <Icon type="upload" /> Select image
+                                </Button>
+                            </Upload>
+                            <Button
+                                type="primary"
+                                onClick={this.handleUpload}
+                                disabled={file === ''}
+                                loading={uploading}
+                                style={{ marginTop: 16 }}
+                            >
+                                {uploading ? 'Uploading' : 'Start Upload' }
                             </Button>
-                        </Upload>
+                        </React.Fragment>
                 )}
                 </Form.Item>
             </Form>
